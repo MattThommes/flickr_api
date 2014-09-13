93c58da7190f136c39712e361d2967d881a3261c
Matt Thommes <matt@activecampaign.com>
2014-09-13 11:32:43 -0500

    updated filename to include the datetime stamp. also updated path to save file (for my own use).

--- fetch_photos.php
+++ fetch_photos.php
@@ -12,14 +12,19 @@
 
 	$content = file_get_contents($url);
 	$content = json_decode($content);
-$debug->dbg($content);
+//$debug->dbg($content);
 
 	while ($content->photos->page < $content->photos->pages) {
 		foreach ($content->photos->photo as $photo) {
 			$photo_original_url = "http://farm" . $photo->farm . ".static.flickr.com/" . $photo->server . "/" . $photo->id . "_" . $photo->originalsecret . "_o." . $photo->originalformat;
-			$copy_dir = "/path/to/html/images/directory/";
+			$copy_dir = "/var/www/lighttpd/admin/flickr_api/photos/";
 			$data = file_get_contents($photo_original_url);
-			$file = fopen($copy_dir . $photo->id . "." . $photo->originalformat, "w+");
+			// replace "-" and ":" with nothing. IE: "2014-09-22 08:00:33".
+			$datetaken = preg_replace("/[\-:]/", "", $photo->datetaken);
+			// replace spaces with a dash. IE: "20140922 080033".
+			$datetaken = preg_replace("/[\s]/", "-", $datetaken);
+			$filename = $datetaken . "_" . $photo->id . "." . $photo->originalformat;
+			$file = fopen($copy_dir . $filename, "w+");
 			fputs($file, $data);
 			fclose($file);
 exit; // testing one file.
